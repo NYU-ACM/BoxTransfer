68f056eefbf33ca7e58cf107c30db279a85f97dc
don mennerich <don.mennerich@nyu.edu>
2016-02-04 17:48:12 -0500

    working push to box

--- build.sbt
+++ build.sbt
@@ -1,6 +1,3 @@
 scalaVersion := "2.11.7"
 
-libraryDependencies ++= Seq(
-  "com.box" % "box-java-sdk" % "2.0.0",
-  "com.typesafe" % "config" % "1.2.1"
-)
+libraryDependencies ++= Seq("com.box" % "box-java-sdk" % "2.0.0", "com.typesafe" % "config" % "1.2.1")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,52 @@
 
 import com.box.sdk._
+import com.typesafe.config.ConfigFactory
 import collection.JavaConversions._
 import java.io._
-import com.typesafe.config.ConfigFactory
 
 object Main extends App {
   val conf = ConfigFactory.load() 
   val api = new BoxAPIConnection(conf.getString("box.key"))
-  val rootFolder = BoxFolder.getRootFolder(api)
   
- val incoming = new BoxFolder(api, "6365824785")
- println(incoming.getInfo.getName())
+ 	val incoming = new BoxFolder(api, "6365824785")
+ 	
+ 	val source = new File(args(0))
 
+  val boxRoot = incoming.createFolder(source.getName)
 
-/*
-	def iterate(root: BoxFolder) {
-	  root.getChildren().foreach{ obj => 
-	  	val resource = obj.getResource
-	  	resource match {
-	  		case _: BoxFile => println("  ITEM: " + obj.getName + " " + obj.getID)
-	  		case _: BoxFolder => { 
-	  			println("FOLDER: " + obj.getName + " " + obj.getID) 
-	  			iterate(resource.asInstanceOf[BoxFolder])
-	  		}
-	  		case _ =>
+	iterateFS(source, new BoxFolder(api, boxRoot.getID))
+
+	def iterateFS(root: File, boxFolder: BoxFolder) {
+	  root.listFiles.foreach{ obj =>
+	    obj.isFile match {
+	    	case true => { boxFolder.uploadFile(new FileInputStream(obj), obj.getName) }
+	    	case false => { 
+	    		containsChild(obj.getName, boxFolder) match {
+	    			case true => //this shouldn't happen
+	    			case false => {
+	    				val child = boxFolder.createFolder(obj.getName)
+	    				iterateFS(obj, new BoxFolder(api, child.getID))
+	    			}
+	    		}
+	    	} 
+	    }
+	  }	
+	}
+
+	def containsChild(name: String, root: BoxFolder): Boolean = {
+	  val children = root.getChildren()
+	  var result = false
+
+	  children.foreach { obj =>
+	  	if(result == false) {
+		  	obj.getName.equals(name) match {
+		  		case true => result = true
+		  		case false => result = false
+		  	}
 	  	}
 	  }
+
+	  result
 	}
-	*/
+
 }
