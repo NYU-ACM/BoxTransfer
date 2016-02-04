648114d718242a2983fd5f85d35234f7e8e9911c
don mennerich <don.mennerich@nyu.edu>
2016-02-04 18:04:58 -0500

    removing unneccesarry code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,13 +5,11 @@ import collection.JavaConversions._
 import java.io._
 
 object Main extends App {
+  
   val conf = ConfigFactory.load() 
   val api = new BoxAPIConnection(conf.getString("box.key"))
-  
- 	val incoming = new BoxFolder(api, "6365824785")
- 	
+ 	val incoming = new BoxFolder(api, conf.getString("box.incoming"))
  	val source = new File(args(0))
-
   val boxRoot = incoming.createFolder(source.getName)
 
 	iterateFS(source, new BoxFolder(api, boxRoot.getID))
@@ -20,33 +18,9 @@ object Main extends App {
 	  root.listFiles.foreach{ obj =>
 	    obj.isFile match {
 	    	case true => { boxFolder.uploadFile(new FileInputStream(obj), obj.getName) }
-	    	case false => { 
-	    		containsChild(obj.getName, boxFolder) match {
-	    			case true => //this shouldn't happen
-	    			case false => {
-	    				val child = boxFolder.createFolder(obj.getName)
-	    				iterateFS(obj, new BoxFolder(api, child.getID))
-	    			}
-	    		}
-	    	} 
+	    	case false => { iterateFS(obj, new BoxFolder(api, boxFolder.createFolder(obj.getName).getID)) } 
 	    }
 	  }	
-	}
-
-	def containsChild(name: String, root: BoxFolder): Boolean = {
-	  val children = root.getChildren()
-	  var result = false
-
-	  children.foreach { obj =>
-	  	if(result == false) {
-		  	obj.getName.equals(name) match {
-		  		case true => result = true
-		  		case false => result = false
-		  	}
-	  	}
-	  }
-
-	  result
 	}
 
 }
